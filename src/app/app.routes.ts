9acdf5e078115277d55043544e9d2fec8d092800
FlorFuentes <flopy_200412@hotmail.com>
2025-02-28 20:24:58 -0300

    Add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
-
-
 //Componentes
 import { NavbarComponent } from './components/navbar/navbar.component';
-
+import { HeroComponent } from './components/hero/hero.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 export const routes: Routes = [
-    /* { path: "", redirectTo: '/inicio', pathMatch: 'full' }, */
-    /* { path: 'navbar', component:NavbarComponent } */
+    { path: "", redirectTo: '/inicio', pathMatch: 'full' },
+    { path: 'navbar', component: NavbarComponent },
+    { path: 'hero', component: HeroComponent },
+    { path: 'sobremi', component: SobreMiComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'footer', component: FooterComponent }
 ];
